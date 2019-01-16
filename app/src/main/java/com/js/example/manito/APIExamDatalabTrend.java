e56ba29948b097740071187c0e5f4b338408ab91
hy38 <sanghyun12101@gmail.com>
2019-01-16 19:15:20 +0900

    1/16 7:15 Max

--- app/src/main/java/com/js/example/manito/APIExamDatalabTrend.java
+++ app/src/main/java/com/js/example/manito/APIExamDatalabTrend.java
@@ -5,6 +5,8 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class APIExamDatalabTrend {
 
@@ -13,8 +15,18 @@ public class APIExamDatalabTrend {
         String clientSecret = "lqvMc6UEBC";//애플리케이션 클라이언트 시크릿 값";
 
         try {
+            SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+            Calendar cal = Calendar.getInstance();
+
+            cal.add(cal.DATE, -1);
+            String yesterday = date.format(cal.getTime());
+//            System.out.println(yesterday);
+
             String apiURL = "https://openapi.naver.com/v1/datalab/shopping/categories";
-            String body = "{\"startDate\":\"2017-08-01\",\"endDate\":\"2017-09-30\",\"timeUnit\":\"date\",\"category\":[{\"name\":\"패션의류\",\"param\":[\"50000000\"]},{\"name\":\"화장품/미용\",\"param\":[\"50000002\"]}],\"device\":\"pc\",\"ages\":[\"20\",\"30\"],\"gender\":\"f\"}";
+//            유저가 변경할 수 있는 검색조건사항들 : 카테고리
+//            검색은 무조건 어제날짜까지 1달간격으로 진행됨.
+//            String body = "{\"startDate\":\"2018-08-01\",\"endDate\":\"2019-01-15\",\"timeUnit\":\"month\",\"category\":[{\"name\":\"패션의류\",\"param\":[\"50000000\"]},{\"name\":\"화장품/미용\",\"param\":[\"50000002\"]}],\"device\":\"mo\",\"ages\":[\"20\",\"30\"],\"gender\":\"\"}";
+            String body = "{\"startDate\":\"2018-08-01\",\"endDate\":\""+yesterday+"\", \"timeUnit\":\"month\",\"category\":[{\"name\":\"패션의류\",\"param\":[\"50000000\"]},{\"name\":\"화장품/미용\",\"param\":[\"50000002\"]}],\"device\":\"mo\",\"ages\":[\"20\",\"30\"],\"gender\":\"\"}";
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("POST");
