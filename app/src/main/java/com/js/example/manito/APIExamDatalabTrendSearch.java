628a3101b3b1ae1485fd7e92db33a770280acc44
hy38 <sanghyun12101@gmail.com>
2019-01-16 15:42:31 +0900

    3:42

---
+++ app/src/main/java/com/js/example/manito/APIExamDatalabTrendSearch.java
@@ -0,0 +1,51 @@
+package com.js.example.manito;
+
+import java.io.BufferedReader;
+        import java.io.DataOutputStream;
+        import java.io.InputStreamReader;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
+
+public class APIExamDatalabTrendSearch {
+
+    public static void main(String[] args) {
+        String clientId = "YOUR_CLIENT_ID";//애플리케이션 클라이언트 아이디값";
+        String clientSecret = "YOUR_CLIENT_SECRET";//애플리케이션 클라이언트 시크릿값";
+
+        try {
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/datalab/v1/search";
+            String body = "{\"startDate\":\"2017-01-01\",\"endDate\":\"2017-04-30\",\"timeUnit\":\"month\",\"keywordGroups\":[{\"groupName\":\"한글\",\"keywords\":[\"한글\",\"korean\"]},{\"groupName\":\"영어\",\"keywords\":[\"영어\",\"english\"]}],\"device\":\"pc\",\"ages\":[\"1\",\"2\"],\"gender\":\"f\"}";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", clientId);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", clientSecret);
+            con.setRequestProperty("Content-Type", "application/json");
+
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.write(body.getBytes());
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 에러 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+            System.out.println(response.toString());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
--- app/src/main/java/com/js/example/manito/MainActivity.java
+++ app/src/main/java/com/js/example/manito/MainActivity.java
@@ -1,13 +1,47 @@
 package com.js.example.manito;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("알림", "확인");
+//                NaverShoppingTask mNaverShoppingTask = new NaverShoppingTask();
+//                mNaverShoppingTask.execute();
+
+
+            }
+        });
+
+    }
+
+    public class NaverShoppingTask extends AsyncTask<String[], Void, String>{
+
+        @Override
+        public String doInBackground(String[]... String){
+//            APIExamDatalabTrend.main(new String[]{""});
+            return null;
+
+        }
+
+        @Override
+        public void onPostExecute(String result){super.onPostExecute(result);}
     }
 }
