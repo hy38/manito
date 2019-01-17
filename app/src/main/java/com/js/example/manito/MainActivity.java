c3c9efb75032d5f80eec472be321fc704353c3af
hy38 <sanghyun12101@gmail.com>
2019-01-17 16:13:17 +0900

    1/17 4:13

--- app/src/main/java/com/js/example/manito/MainActivity.java
+++ app/src/main/java/com/js/example/manito/MainActivity.java
@@ -10,10 +10,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 64de9e1e785ac147ab0c0ca9046532af5f04fd3f
     Button btn;
 
     @Override
@@ -26,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.v("알림", "확인");
-//                NaverShoppingTask mNaverShoppingTask = new NaverShoppingTask();
-//                mNaverShoppingTask.execute();
+                NaverShoppingTask mNaverShoppingTask = new NaverShoppingTask();
+                mNaverShoppingTask.execute();
 
             }
 
@@ -37,40 +34,19 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
 
-<<<<<<< HEAD
-   /* public class NaverShoppingTask extends AsyncTask<String[], Void, String> {
-//  Parameters : AsyncTask<doInBackground()의 변수 종류, onProgressUpdate()에서 사용할 변수 종류, onPostExecute()에서 사용할 변수종류>
+    public class NaverShoppingTask extends AsyncTask<String[], Void, String> {
+        //  Parameters : AsyncTask<doInBackground()의 변수 종류, onProgressUpdate()에서 사용할 변수 종류, onPostExecute()에서 사용할 변수종류>
         @Override
         public String doInBackground(String[]... String) {
             APIExamDatalabTrend.main(new String[]{"10"});
             return null;
-=======
-/*
-        public class NaverShoppingTask extends AsyncTask<String[], Void, String> {
-
-            @Override
-            public String doInBackground(String[]... String) {
-//            APIExamDatalabTrend.main(new String[]{""});
-                return null;
-
-            }
->>>>>>> 64de9e1e785ac147ab0c0ca9046532af5f04fd3f
-
-            @Override
-            public void onPostExecute(String result) {
-                super.onPostExecute(result);
-            }
         }
 
-<<<<<<< HEAD
         @Override
         public void onPostExecute(String result) {
             super.onPostExecute(result);
         }
-    }*/
-=======
-    */
     }
->>>>>>> 64de9e1e785ac147ab0c0ca9046532af5f04fd3f
 }
