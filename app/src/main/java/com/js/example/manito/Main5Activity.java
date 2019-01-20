eac6fa6259351f79696a79062ddc4ea0f533a019
Choyunhui <a01022371341@gmail.com>
2019-01-20 16:28:58 +0900

    나도결제 구현 스타트!

---
+++ app/src/main/java/com/js/example/manito/Main5Activity.java
@@ -0,0 +1,24 @@
+package com.js.example.manito;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+
+        public void onbuy(View v){
+            //결제 처리
+
+
+        }
+
+
+    }
+
+
